7c6afaa4cb65568af9332ce07d62980c1d418f56
Isadorafonseeca <mifm@discente.ifpe.edu.br>
2026-03-26 10:21:19 -0300

    Add files via upload

---
+++ atividade/Color.kt
@@ -0,0 +1,11 @@
+package com.example.composenavigationapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xEDEEE27D)
+val PurpleGrey80 = Color(0xFFF6DE08)
+val Pink80 = Color(0xFFF8F0AD)
+
+val Purple40 = Color(0xFFFFEB3B)
+val PurpleGrey40 = Color(0xFFE7D112)
+val Pink40 = Color(0xFFEAD518)
